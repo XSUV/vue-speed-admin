614d841130029034cf259c71ea59f060a3a97408
XSUV <1247717837@qq.com>
2024-05-11 11:52:05 +0800

    fix: prettier config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,12 @@ module.exports = {
   extends: ['speed/typescript/vue', 'plugin:prettier/recommended'],
   ignorePatterns: ['dist/*', 'public/*', 'src/assets/**', 'src/**/iconfont/**', '*.config.js'],
   rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
     'no-undef': 'off',
     'import/no-cycle': 'off',
     '@typescript-eslint/no-shadow': 'off',
